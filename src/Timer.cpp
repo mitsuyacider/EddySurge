ed36fc18aa89ea022eecb4efbf527621159a3d74
mw <mw@mw-no-MacBook-Air.local>
2017-04-04 16:23:12 +0900

    Fixed memory leak

---
+++ src/Timer.cpp
@@ -0,0 +1,9 @@
+//
+//  Timer.cpp
+//  EddySurge
+//
+//  Created by Mitstuya.WATANABE on 2017/04/03.
+//
+//
+
+#include "Timer.hpp"
---
+++ src/Timer.hpp
@@ -0,0 +1,14 @@
+//
+//  Timer.hpp
+//  EddySurge
+//
+//  Created by Mitstuya.WATANABE on 2017/04/03.
+//
+//
+
+#ifndef Timer_hpp
+#define Timer_hpp
+
+#include <stdio.h>
+
+#endif /* Timer_hpp */
